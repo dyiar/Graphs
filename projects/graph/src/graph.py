56b0cec9ee124e083eafd01033a4426ddb7e7804
dyiar <austin.hl.blake@gmail.com>
2019-04-15 13:50:47 -0600

    bft done

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,6 +1,7 @@
 """
 Simple graph implementation
 """
+from queue import Queue
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -12,8 +13,30 @@ class Graph:
         
 
     def add_edge(self, v1, v2):
-        self.vertices[v1].add(v2)
-        self.vertices[v2].add(v1)
+        if v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("Vertex doesn't exist")
+
+    def bft(self, start_vert):
+
+        q = Queue()
+
+        q.put(start_vert)
+        
+        visited = set()
+
+        while q.qsize() > 0:
+            v = q.get()
+            if v not in visited:
+                visited.add(v)
+                for next_vert in self.vertices[v]:
+                    q.put(next_vert)
+
+        print(visited)
+
+
         
 
 
@@ -24,4 +47,10 @@ graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
-print(graph.vertices)
+graph.add_edge('2', '1')
+graph.add_edge('2', '0')
+graph.add_edge('2', '3')
+# print(graph.vertices)
+# Continuing from previous example
+# graph.add_edge('0', '4')  # No '4' vertex, should raise an Exception!
+graph.bft('0')
